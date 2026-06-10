1f8b75e565bb06bf443c06030d6225fcee90e1dc
T-ramya-sree <85395315+T-ramya-sree@users.noreply.github.com>
2026-06-10 12:39:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function convertToRoman(num) {
-  	const obj = {
-      0:['M',1000], 
-      1:['D', 500], 
-      2:['C', 100], 
-      3:['L', 50], 
-      4:['X', 10], 
-      5:['V', 5], 
-      6:['I', 1]
-    };
+  	const romanMap= {
+	     ['M',1000], 
+         ['D', 500], 
+         ['C', 100], 
+         ['L', 50], 
+         ['X', 10], 
+         ['V', 5], 
+         ['I', 1]
+	};
+      
 
   //your code here
 let result="";
